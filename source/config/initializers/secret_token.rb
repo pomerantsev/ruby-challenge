9ce99b718e945668833ce281ba3e6d48795367c7
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-01-18 14:04:29 +0400

    Add source folder

---
+++ source/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KanbanBoard::Application.config.secret_key_base = '04d58085419b7b0f70c8cdb5ef4b64611f3f161fa92b9a4e1a2249cfffd13a19b16e0a38d7877030821c2ddcf1d2d3356363431810c02169f7ad076d4d63b782'
