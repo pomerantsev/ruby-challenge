226ed931c561b695fe66d973cfc0e52c48cc8e2e
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-01-18 14:08:34 +0400

    Show empty issues array

--- source/config/routes.rb
+++ source/config/routes.rb
@@ -1,4 +1,5 @@
 KanbanBoard::Application.routes.draw do
+  root "issues#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
