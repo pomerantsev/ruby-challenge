3353d76871c0acd2da9901da63a1128975f30e18
Pavel Pomerantsev <pomerantsevp@gmail.com>
2014-01-18 14:15:31 +0400

    Fetch last 50 issues

--- source/app/controllers/issues_controller.rb
+++ source/app/controllers/issues_controller.rb
@@ -1,5 +1,13 @@
 class IssuesController < ApplicationController
   def index
-    @issues = []
+    issues_by_state = {}
+    %i(open closed).each do |state|
+      issues_by_state[state] = HTTParty.get "https://api.github.com/repos/sinatra/sinatra/issues?state=#{state}&per_page=50",
+                                            headers: { 'User-Agent' => 'pomerantsev' }
+    end
+    @all_issues = (issues_by_state[:open] + issues_by_state[:closed])
+      .sort_by { |issue| issue['created_at'] }
+      .last(50)
+      .reverse
   end
 end
